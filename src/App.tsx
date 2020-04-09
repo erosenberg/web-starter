32c69a1b42577c683213e21a4d501f514398bc75
Eitan <eitanrosenberg@gmail.com>
2020-04-09 16:54:19 -0500

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { hot } from 'react-hot-loader/root';
+import React, { useState } from 'react';
+
+import './styles/index.scss';
+
+
+export const App = () => {
+  return (<div>Hello World</div>);
+}
+
+export default hot(App);
